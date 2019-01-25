fb0585afd46a15bb09037e9263969ed1b7b53a95
Simone Tiraboschi <stirabos@redhat.com>
2019-01-25 14:46:12 +0100

    execute linters only if available

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -4,9 +4,17 @@
 rm -rf output
 rm -f ./*tar.gz
 
-# Run linters
-ansible-lint .
-yamllint .
+# Run linters if available
+if [ -x "$(command -v ansible-lint)" ] ; then
+    ansible-lint .
+else
+    echo "Skipping ansible-lint because it's not available"
+fi
+if [ -x "$(command -v yamllint)" ] ; then
+    yamllint .
+else
+    echo "Skipping yamllint because it's not available"
+fi
 
 # Get the tarball
 ./build.sh dist
