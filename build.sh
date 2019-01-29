5e928a1d9fef3c90a45df7c55ed5f0e7207218f3
Simone Tiraboschi <stirabos@redhat.com>
2019-01-29 09:53:59 +0100

    build: post ovirt-ansible-hosted-engine-setup-1.0.8

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION="1.0.8"
-MILESTONE=
-RPM_RELEASE="1"
+VERSION="1.0.9"
+MILESTONE=master
+RPM_RELEASE="0.1.$MILESTONE.$(date -u +%Y%m%d%H%M%S)"
 
 ROLE_NAME="ovirt.hosted_engine_setup"
 PACKAGE_NAME="ovirt-ansible-hosted-engine-setup"
